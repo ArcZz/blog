1269b339c34c5d85ae033ad6f25e81267a9ba417
Zhang Zhi <Zhang48240@gmail.com>
2022-11-08 17:25:11 +0800

    d

--- myblog/src/App.vue
+++ myblog/src/App.vue
@@ -1,19 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-link to="/">home</router-link>
+    <router-link to="/about">about</router-link>
+    <router-link to="/contact">contact</router-link>
+
+
+    <HelloWorld   :path="`${path}`" :fileName="`${name}`"/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import "./assets/theme/index.css";
+import ElementUI from 'element-ui';
+import Vue from 'vue';
+
+
+Vue.use(ElementUI);
 
 export default {
   name: 'App',
   components: {
     HelloWorld
+  },
+  data () {
+    return {
+      name: 'zz.pdf',
+      path: 'lib/web/viewer.html'
+    }
   }
 }
+
+
 </script>
 
 <style>
@@ -23,6 +41,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 10px;
 }
 </style>
